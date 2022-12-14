72a2b5f2919fef66b59e0190a69b6db4acb11deb
owlman <savva-fokin2002@mail.ru>
2022-12-15 02:59:09 +0300

    task12 fucking dies

--- task12/main.py
+++ task12/main.py
@@ -15,8 +15,8 @@ def letters_to_nums(lines):
                 height_ln.append(1)
                 height_ln2.append(1)
             elif k == 'E':
-                height_ln.append(27)
-                height_ln2.append(27)
+                height_ln.append(26)
+                height_ln2.append(26)
             else:
                 if abc.find(k)==25:
                     print(k)
@@ -103,24 +103,103 @@ nbrs_of_nbrs = graph[0]
 ct = 0
 nbrs = []
 pp = 0
-queue = [coord_translator_reverse(*start_point)]
-visited = []
-dists = {coord_translator_reverse(*start_point): 0}
+
 print('SAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', coord_translator_reverse(*end_point))
 
 
+def itera(start_point):
+    queue = [coord_translator_reverse(*start_point)]
+    visited = []
+    dists = {coord_translator_reverse(*start_point): 0}
+    prs={}
+    while queue != []:
+        # print(queue)
+        el = queue.pop(0)
+        if el in graph:
+            for i in graph[el]:
+                if i not in visited:
+                    dists[i] = dists[el] + 1
+                    visited.append(i)
+                    queue.append(i)
+                    prs[i]=el
+                    if i == 3358:
+                        res_path = []
+                        cur_vertex = 3358
+                        while cur_vertex != coord_translator_reverse(*start_point):
+                            res_path.append(cur_vertex)
+                            cur_vertex = prs[cur_vertex]
+                        res_path = list(reversed(res_path))
+                        if True:
+                            print(res_path)
+                            print(' '.join(map(lambda x: str(height_mx[coord_translator(x)[1]][coord_translator(x)[0]]), res_path)))
+                            #abc(x)[1], abc(x)[0]
+                        return dists[3358]
 
-while queue != []:
-    # print(queue)
-    el = queue.pop(0)
-    for i in graph[el]:
-        if i not in visited:
-            dists[i] = dists[el] + 1
+    return 10000
 
-            visited.append(i)
-            queue.append(i)
-            if i == 3519:
-                break
+import queue
+import typing
 
-print(coord_translator(3519))
-print(dists[3519])
+T = typing.TypeVar('T')
+
+
+def bfs(
+        connections_list: typing.Dict[T, typing.List[T]],
+        start: T,
+        target: T,
+) -> typing.Tuple[typing.Dict[T, T], typing.Dict[T, int], bool]:
+    print(start, target)
+    vertexes_queue = queue.Queue()
+    visited = {start: True}
+    dists_from_start = {start: 0}
+    parents = {}
+    vertexes_queue.put(start)
+    while not vertexes_queue.empty():
+        checking_vertex = vertexes_queue.get()
+        #print('AAAA', connections_list, connections_list[checking_vertex])
+        if checking_vertex not in connections_list:  # some vertexes dont have any connections
+            continue
+        for neighbour_vertex in connections_list[checking_vertex]:
+            if not visited.get(neighbour_vertex, False):
+                visited[neighbour_vertex] = True
+                dists_from_start[neighbour_vertex] = dists_from_start[checking_vertex] + 1
+                parents[neighbour_vertex] = checking_vertex
+                vertexes_queue.put(neighbour_vertex)
+                if neighbour_vertex == target:
+                    return parents, dists_from_start, True
+    return parents, dists_from_start, False
+
+
+def construct_path_from_bfs(paths: typing.Dict[int, T], start: T, target: T) -> typing.List[T]:
+    res_path = []
+    cur_vertex = target
+    while cur_vertex != start:
+        res_path.append(cur_vertex)
+        print(coord_translator(cur_vertex), height_mx[coord_translator(cur_vertex)[1]][coord_translator(cur_vertex)[0]])
+        cur_vertex = paths[cur_vertex]
+    res_path = list(reversed(res_path))
+    return res_path
+
+
+
+min=10000
+
+for i in graph:
+    start_point=coord_translator(i)
+    if height_mx[start_point[1]][start_point[0]]==1:
+        print(min)
+        print(start_point, height_mx[start_point[1]][start_point[0]])
+        prs, dist, exists=bfs(graph.copy(), i, 3358)
+        print(graph[i])
+        print(exists)
+        if exists and dist[3358]<min:
+            y=prs
+            x=i
+            min=dist[3358]
+
+print('as')
+print(prs)
+print(construct_path_from_bfs(y, x, 3358))
+print(min)
+
+print(coord_translator(3358))
